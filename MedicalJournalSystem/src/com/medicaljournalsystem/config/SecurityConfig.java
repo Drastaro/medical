5cf5f2224db843c88f6e0de857f2a83f2d98cbd9
Mircea <Mircea@Dell>
2016-04-24 01:05:28 +0300

    updated security for REST

--- MedicalJournalSystem/src/com/medicaljournalsystem/config/SecurityConfig.java
+++ MedicalJournalSystem/src/com/medicaljournalsystem/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.medicaljournalsystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,19 +23,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers("/", "/home").authenticated().antMatchers("/users/list")
-				.hasAuthority("ROLE_PUBLISHER").antMatchers("/users/add").hasAuthority("ROLE_PUBLISHER")
-				.antMatchers("/users/delete/*").hasAuthority("ROLE_PUBLISHER").antMatchers("/medicaljournals/list")
-				.hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/add").hasAuthority("ROLE_PUBLISHER")
-				.antMatchers("medicaljournals/edit/*").hasAuthority("ROLE_PUBLISHER").antMatchers("rest/journals")
-				.hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/submit").hasAuthority("ROLE_PUBLISHER")
-				.antMatchers("medicaljournals/search").authenticated().antMatchers("/medicaljournals/subscribe")
-				.authenticated().and().formLogin().loginPage("/login").loginProcessingUrl("/j_spring_security_check")
-				.usernameParameter("email").passwordParameter("password").and().csrf().and().exceptionHandling()
-				.accessDeniedPage("/500");
-
+	@Configuration
+	@Order(1)
+	public static class ApiWebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable().antMatcher("/rest/**").authorizeRequests().anyRequest().authenticated().and()
+					.httpBasic();
+		}
 	}
+
+	@Configuration
+	@Order(2)
+	public static class FormWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().antMatchers("/", "/home").authenticated().antMatchers("/users/list")
+					.hasAuthority("ROLE_PUBLISHER").antMatchers("/users/add").hasAuthority("ROLE_PUBLISHER")
+					.antMatchers("/users/delete/*").hasAuthority("ROLE_PUBLISHER").antMatchers("/medicaljournals/list")
+					.hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/add").hasAuthority("ROLE_PUBLISHER")
+					.antMatchers("medicaljournals/edit/*").hasAuthority("ROLE_PUBLISHER").antMatchers("rest/journals")
+					.hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/submit").hasAuthority("ROLE_PUBLISHER")
+					.antMatchers("medicaljournals/search").authenticated().antMatchers("/medicaljournals/subscribe")
+					.authenticated().and().formLogin().loginPage("/login")
+					.loginProcessingUrl("/j_spring_security_check").usernameParameter("email")
+					.passwordParameter("password").and().csrf().and().exceptionHandling().accessDeniedPage("/500");
+		}
+	}
+
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	//
+	// http.authorizeRequests().antMatchers("/",
+	// "/home").authenticated().antMatchers("/users/list")
+	// .hasAuthority("ROLE_PUBLISHER").antMatchers("/users/add").hasAuthority("ROLE_PUBLISHER")
+	// .antMatchers("/users/delete/*").hasAuthority("ROLE_PUBLISHER").antMatchers("/medicaljournals/list")
+	// .hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/add").hasAuthority("ROLE_PUBLISHER")
+	// .antMatchers("medicaljournals/edit/*").hasAuthority("ROLE_PUBLISHER").antMatchers("rest/journals")
+	// .hasAuthority("ROLE_PUBLISHER").antMatchers("medicaljournals/submit").hasAuthority("ROLE_PUBLISHER")
+	// .antMatchers("medicaljournals/search").authenticated().antMatchers("/medicaljournals/subscribe")
+	// .authenticated().and().formLogin().loginPage("/login").loginProcessingUrl("/j_spring_security_check")
+	// .usernameParameter("email").passwordParameter("password").and().csrf().and().exceptionHandling()
+	// .accessDeniedPage("/500");
+	//
+	// }
 }
--- MedicalJournalSystem/src/com/medicaljournalsystem/controller/MedicalJournalRestController.java
+++ MedicalJournalSystem/src/com/medicaljournalsystem/controller/MedicalJournalRestController.java
@@ -1,10 +1,13 @@
 package com.medicaljournalsystem.controller;
 
-import java.util.List;
+import java.security.Principal;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.medicaljournalsystem.dao.MedicalJournalDAO;
 import com.medicaljournalsystem.dao.UserDAO;
 import com.medicaljournalsystem.pojo.MedicalJournal;
+import com.medicaljournalsystem.pojo.Users;
 
 @Controller
 @RequestMapping(value = "rest")
@@ -24,10 +28,26 @@ public class MedicalJournalRestController {
 
 	@ResponseBody
 	@RequestMapping(value = "/journals", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<MedicalJournal> listJournals() {
+	public Set<MedicalJournal> listJournals(Principal principal) {
 
-		List<MedicalJournal> listJournals = medicalJournalDao.list();
+		Users currentUser = userDao.getByEmail(principal.getName());
+		if (currentUser == null) {
+			return null;
+		}
+
+		Set<MedicalJournal> listJournals = currentUser.getSubscribedMedicalJournals();
 		return listJournals;
+	}
+
+	@RequestMapping(value = "/journals/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public FileSystemResource getFile(@PathVariable("id") int journalId) {
+		// get the journal from DB
+		MedicalJournal journal = medicalJournalDao.get(journalId);
+		// see if the user is subscribed to the journal
+		// TODO
+		// if all good then return the file content
+		return new FileSystemResource(journal.getFilePath());
 	}
 
 }
